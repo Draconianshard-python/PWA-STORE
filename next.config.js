0f92203b038b582cd8ec5bd9709c5e048baefc62
Draconianshard-python <draconianshard@icloud.com>
2025-05-28 14:29:38 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'cdn.jsdelivr.net'
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
+}
+
+module.exports = withPWA(nextConfig)
